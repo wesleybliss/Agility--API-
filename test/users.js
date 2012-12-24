c42763bdb5a72fc425b54b7aeaab12101c476860
Wesley Bliss <wes@fueled.com>
2012-12-23 19:33:20 -0500

    Added tests using should.js, supertest, and Mocha (but couldn't get them working yet.

---
+++ test/users.js
@@ -0,0 +1,12 @@
+
+var app = require('../app.js');
+ 
+describe('GET /users', function(){
+  it('respond with json', function(done){
+    request(app)
+      .get('/user')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200, done);
+  });
+});
